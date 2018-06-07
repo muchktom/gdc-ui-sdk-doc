ff2b63df0ff0ab9e724eed584618841ece8abbec
Tomáš Muchka <tomas.muchka@gooddata.com>
2018-06-07 17:09:55 +0200

    RELATED: RAIL-945: Add GA events for selected items on GoodData.UI homepage

--- website/static/js/toggleNav.js
+++ website/static/js/toggleNav.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     var navToggler = document.getElementById('navToggler');
 
-    navToggler.addEventListener('click', function() {
-        document.body.classList.toggle('toggleNavOpen');
-    });
+    if(navToggler) {
+    	navToggler.addEventListener('click', function() {
+	        document.body.classList.toggle('toggleNavOpen');
+	    });
+    }
 });
